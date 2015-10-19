57a745b64520df42fc319770bf50db26f322d3cd
walidowais <walidowais@gmail.com>
2015-10-19 02:41:11 -0500

    simple fizzbuzz print 1 to 100

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,15 @@
 # Walid Owais
 # Headspring skills assessment
 # FizzBuzz
+
+i = 1
+while i <= 100:
+  if (i % 3 == 0) and (i % 5 == 0):
+    print "FizzBuzz"
+  elif i % 3 == 0:
+    print "Fizz"
+  elif i % 5 == 0:
+    print "Buzz"
+  else:
+    print i
+  i += 1
